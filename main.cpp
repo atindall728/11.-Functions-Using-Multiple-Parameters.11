b6cb47f9e556d9e7c5c496f43429fbc6c8edc970
atindall728 <atindall728@gmail.com>
2015-03-25 17:30:15 -0500

    The New Boston C++ - Functions Using Parameters
    The New Boston Source Code For C++

---
+++ main.cpp
@@ -0,0 +1,27 @@
+// The New Boston - / //
+// 11. Functions Using Multiple Parameters //
+
+#include <iostream>
+using namespace std;
+
+
+void printCrap(int x) {  // void means to return nothing (program reads from top down)
+
+
+	cout << " bucky's favorite number is " << x << endl;
+
+};
+
+
+int main()
+
+{
+
+	printCrap(20);
+
+	cin.ignore().get();   //  If your code does not pause automatically on screen use this getline to view program;
+
+	return 0;  // This will run your program and display to screen
+}
+
+// Questions about this page?  Browse the corresponding forum @ thenewboston.com or Adam Tindall Via The New Boston//
